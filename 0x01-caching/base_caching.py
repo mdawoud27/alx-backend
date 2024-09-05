de4572ada054f3e5e4f7cf6ffa530ca210837ef2
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-05 18:26:37 +0300

    Update `.gitignore` file and fix `base_caching.py` file

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -2,7 +2,8 @@
 """ BaseCaching module
 """
 
-class BaseCaching():
+
+class BaseCaching:
     """ BaseCaching defines:
       - constants of your caching system
       - where your data are stored (in a dictionary)
@@ -10,23 +11,19 @@ class BaseCaching():
     MAX_ITEMS = 4
 
     def __init__(self):
-        """ Initiliaze
-        """
+        """ Initialiaze"""
         self.cache_data = {}
 
     def print_cache(self):
-        """ Print the cache
-        """
+        """ Print the cache"""
         print("Current cache:")
         for key in sorted(self.cache_data.keys()):
             print("{}: {}".format(key, self.cache_data.get(key)))
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """ Add an item in the cache"""
         raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """ Get an item by key"""
         raise NotImplementedError("get must be implemented in your cache class")
